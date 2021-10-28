53f86a5ce6aa358ed3e0f6fafd8ae4d553339105
Thiago <thiago12prata@gmail.com>
2021-10-28 12:54:28 -0300

    implementado os movimentos possiveis do rei

--- src/xadrez/pecas/Rei.java
+++ src/xadrez/pecas/Rei.java
@@ -1,5 +1,6 @@
 package xadrez.pecas;
 
+import tabuleiro.Posicao;
 import tabuleiro.Tabuleiro;
 import xadrez.Cor;
 import xadrez.PecaXadrez;
@@ -18,8 +19,56 @@ public class Rei extends PecaXadrez{
 	@Override
 	public boolean[][] movimentosPossiveis() {
 		boolean[][] mat = new boolean[getTabuleiro().getLinha()][getTabuleiro().getColuna()];	
+		
+		Posicao p = new Posicao(0, 0);
+		
+		//acima 
+		p.setValores(posicao.getLinha() - 1, posicao.getColuna());
+		if(getTabuleiro().existePosicao(p) && podeMover(p)) {
+			mat[p.getLinha()][p.getColuna()] = true;
+		}
+		//abaixo 
+		p.setValores(posicao.getLinha() + 1, posicao.getColuna());
+		if(getTabuleiro().existePosicao(p) && podeMover(p)) {
+			mat[p.getLinha()][p.getColuna()] = true;
+		}
+		//esquerda 
+		p.setValores(posicao.getLinha(), posicao.getColuna() - 1);
+		if(getTabuleiro().existePosicao(p) && podeMover(p)) {
+			mat[p.getLinha()][p.getColuna()] = true;
+		}
+		//direita 
+		p.setValores(posicao.getLinha(), posicao.getColuna() + 1);
+		if(getTabuleiro().existePosicao(p) && podeMover(p)) {
+			mat[p.getLinha()][p.getColuna()] = true;
+		}
+		//NO 
+		p.setValores(posicao.getLinha() - 1, posicao.getColuna() - 1);
+		if(getTabuleiro().existePosicao(p) && podeMover(p)) {
+			mat[p.getLinha()][p.getColuna()] = true;
+		}
+		//NE
+		p.setValores(posicao.getLinha() - 1, posicao.getColuna() + 1);
+		if(getTabuleiro().existePosicao(p) && podeMover(p)) {
+			mat[p.getLinha()][p.getColuna()] = true;
+		}
+		//SO
+		p.setValores(posicao.getLinha() + 1, posicao.getColuna() - 1);
+		if(getTabuleiro().existePosicao(p) && podeMover(p)) {
+			mat[p.getLinha()][p.getColuna()] = true;
+		}
+		//SE
+		p.setValores(posicao.getLinha() + 1, posicao.getColuna() + 1);
+		if(getTabuleiro().existePosicao(p) && podeMover(p)) {
+			mat[p.getLinha()][p.getColuna()] = true;
+		}
+		
 		return mat;
 	}
-
+	
+	private boolean podeMover(Posicao posicao) {
+		PecaXadrez p = (PecaXadrez) getTabuleiro().getPeca(posicao);
+		return p == null || getCor() != p.getCor();
+	}
 	
 }
